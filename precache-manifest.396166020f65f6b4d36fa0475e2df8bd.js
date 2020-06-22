8bdca43f9fd789063b6eda9b7c36168ebe309224
jane0112 <ninja9239@gmail.com>
2020-06-22 15:05:21 +0800

    Updates

---
+++ precache-manifest.396166020f65f6b4d36fa0475e2df8bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a9aeaa90cf4a9e59bace682a7354e2",
+    "url": "/jane0112/accounting-app.git/index.html"
+  },
+  {
+    "revision": "a3298cb053aa15efec2c",
+    "url": "/jane0112/accounting-app.git/static/css/main.3f0fe818.chunk.css"
+  },
+  {
+    "revision": "3c184a1885bd79e25cd2",
+    "url": "/jane0112/accounting-app.git/static/js/2.f2bea58f.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/jane0112/accounting-app.git/static/js/2.f2bea58f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3298cb053aa15efec2c",
+    "url": "/jane0112/accounting-app.git/static/js/main.e65804cc.chunk.js"
+  },
+  {
+    "revision": "1035737998bbfd95c8cb",
+    "url": "/jane0112/accounting-app.git/static/js/runtime-main.ee8a248f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/jane0112/accounting-app.git/precache-manifest.396166020f65f6b4d36fa0475e2df8bd.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jane0112/accounting-app.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
